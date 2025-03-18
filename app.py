247940c4c42126167a629774d1f9d24063aa0fd0
Evdimid18901994 <99124478+Evdimid18901994@users.noreply.github.com>
2025-03-18 19:02:35 +0300

    second

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import av
 import cv2
 import numpy as np
 from ultralytics import YOLO
+import asyncio
+
+# Решение проблемы с asyncio
+try:
+    asyncio.get_running_loop()
+except RuntimeError:
+    asyncio.set_event_loop(asyncio.new_event_loop())
 
 # Загрузка модели YOLOv8
 model = YOLO("yolov8n.pt")
-
 
 # Класс для обработки видеопотока
 class YOLOVideoProcessor(VideoProcessorBase):
@@ -16,10 +22,9 @@ class YOLOVideoProcessor(VideoProcessorBase):
 
         # Обработка кадра с YOLOv8
         results = model.predict(img, conf=0.5)
-        processed_img = results[0].plot()
+        processed_img = cv2.cvtColor(results[0].plot(), cv2.COLOR_RGB2BGR)  # Исправленный вывод
 
         return av.VideoFrame.from_ndarray(processed_img, format="bgr24")
-
 
 # Интерфейс Streamlit
 st.title("YOLOv8 Object Detection via WebRTC")
